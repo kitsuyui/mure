c2a868c89ef572e0f588cbc6a5cdabdd37eb4f28
kitsuyui <kitsuyui@kitsuyui.com>
2022-08-12 04:44:19 +0900

    complete refresh subcommand

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,13 @@ fn main() {
     let cmd = parser();
     let matches = cmd.get_matches();
     match matches.subcommand() {
-        Some(("refresh", _)) => {
-            // TODO: use match matches.subcommand()
-            // matches;
+        Some(("refresh", matches)) => {
             let current_dir = std::env::current_dir().unwrap();
-            match refresh::command::refresh(current_dir.to_str().unwrap()) {
+            let repo_path = match matches.get_one::<String>("repository") {
+                Some(repo) => repo.to_string(),
+                None => current_dir.to_str().unwrap().to_string(),
+            };
+            match refresh::command::refresh(&repo_path) {
                 Ok(_) => (),
                 Err(e) => println!("{}", e),
             }
@@ -37,7 +39,12 @@ fn main() {
 /// Parser
 fn parser() -> App<'static> {
     // TODO: subcommand "init" to create ~/.mure.toml
-    let subcommand_refresh = App::new("refresh").about("refresh repository");
+    let subcommand_refresh = App::new("refresh").about("refresh repository").arg(
+        clap::Arg::with_name("repository")
+            .help("repository to refresh. if not specified, current directory is used")
+            .required(false)
+            .index(1),
+    );
     let subcommand_issues = App::new("issues").about("show issues");
     let cmd = clap::Command::new("mure")
         .bin_name("mure")
