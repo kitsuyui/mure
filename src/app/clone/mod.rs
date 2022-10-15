5a4f9d9bab0b6bc85254c7d84031152298869f61
kitsuyui <kitsuyui@kitsuyui.com>
2022-10-16 05:18:13 +0900

    mure clone create directory if not exist.
    Close: https://github.com/kitsuyui/mure/issues/21

--- src/app/clone/mod.rs
+++ src/app/clone/mod.rs
@@ -1,7 +1,8 @@
 use crate::config::Config;
 use crate::github::repo::RepoInfo;
 use crate::{config::ConfigSupport, mure_error::Error};
-use std::os::unix::fs;
+use std::fs as std_fs;
+use std::os::unix::fs as unix_fs;
 use std::process::Command;
 
 pub fn clone(config: &Config, repo_url: &str) -> Result<(), Error> {
@@ -12,9 +13,8 @@ pub fn clone(config: &Config, repo_url: &str) -> Result<(), Error> {
     let repo_info = parsed.unwrap();
     let tobe_clone = config.repo_store_path(&repo_info.domain, &repo_info.owner, &repo_info.repo);
 
-    if tobe_clone.exists() {
-        return Err(Error::from_str("repo already exists"));
-    }
+    // create dir if not exist (mkdir -p)
+    std_fs::create_dir_all(tobe_clone.as_os_str())?;
 
     let result = Command::new("git")
         .current_dir(tobe_clone.parent().unwrap())
@@ -28,6 +28,36 @@ pub fn clone(config: &Config, repo_url: &str) -> Result<(), Error> {
     }
 
     let link_to = config.repo_work_path(&repo_info.domain, &repo_info.owner, &repo_info.repo);
-    fs::symlink(tobe_clone, link_to).unwrap();
+    unix_fs::symlink(tobe_clone, link_to).unwrap();
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mktemp::Temp;
+
+    #[test]
+    fn test_clone() {
+        let temp_dir = Temp::new_dir().expect("failed to create temp dir");
+        let config_file = format!(
+            r#"
+        [core]
+        base_dir = "{}"
+
+        [github]
+        username = "kitsuyui"
+
+        [shell]
+        cd_shims = "mcd"
+    "#,
+            temp_dir.as_os_str().to_str().unwrap()
+        );
+        let config: Config = toml::from_str(&config_file).unwrap();
+
+        match clone(&config, "https://github.com/kitsuyui/mure") {
+            Ok(_) => {}
+            Err(err) => panic!("{:?}", err),
+        }
+    }
 }
