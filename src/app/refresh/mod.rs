f938230dae5cc606dcf1a4f552ddbcd80a2a9ec1
kitsuyui <kitsuyui@kitsuyui.com>
2022-11-04 10:08:45 +0900

    Fix mure refresh bug
    mure refresh has the feature of removing merged branches.
    However, in some cases it did not work well.
    This pull request fixes it.
    
    Specifically, it means avoiding early returns after judging the repository clean.
    This will allow deletion of previously skipped branches to work properly.

--- src/app/refresh/mod.rs
+++ src/app/refresh/mod.rs
@@ -21,6 +21,7 @@ pub enum Reason {
 }
 
 pub fn refresh(repo_path: &str) -> Result<RefreshStatus, Error> {
+    let mut messages = vec![];
     if !PathBuf::from(repo_path).join(".git").exists() {
         return Ok(RefreshStatus::DoNothing(Reason::NotGitRepository));
     }
@@ -39,15 +40,22 @@ pub fn refresh(repo_path: &str) -> Result<RefreshStatus, Error> {
 
     // TODO: origin is hardcoded. If you have multiple remotes, you need to specify which one to use.
     repo.pull_fast_forwarded("origin", &default_branch)?;
+    messages.push(format!(
+        "Pulled from origin/{} into {}",
+        default_branch, default_branch
+    ));
 
     // switch to default branch if current branch is clean
     if repo.is_clean()? {
         // git switch $default_branch
         let result = repo.switch(&default_branch)?;
-        return Ok(RefreshStatus::Update {
-            switch_to_default: true,
-            message: String::from_utf8(result.stdout).unwrap(),
-        });
+        if result.status.success() {
+            messages.push(format!("Switched to {}", default_branch));
+        } else {
+            let message =
+                String::from_utf8(result.stdout).map_err(|e| Error::from_str(&e.to_string()))?;
+            messages.push(message);
+        }
     }
 
     let merged_branches = repo.merged_branches()?;
@@ -58,11 +66,12 @@ pub fn refresh(repo_path: &str) -> Result<RefreshStatus, Error> {
 
     for branch in delete_branches {
         repo.delete_branch(branch)?;
+        messages.push(format!("Deleted branch {}", branch));
     }
 
     Ok(RefreshStatus::Update {
         switch_to_default: false,
-        message: String::from(""),
+        message: messages.join("\n"),
     })
 }
 
